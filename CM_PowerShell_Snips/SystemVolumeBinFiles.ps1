4bf8bb16784a8a4759cab3196983f912ed00c118
moaiad-ashourizadeh <moaiad.ashourizadeh@gmail.com>
2025-11-24 14:57:31 +0100

    Update comment for Windows 11 compatibility
    Updated comment to reflect compatibility with Windows 11 version 24H2 and suggested change for Get-Volume command.

--- CM_PowerShell_Snips/SystemVolumeBinFiles.ps1
+++ CM_PowerShell_Snips/SystemVolumeBinFiles.ps1
@@ -4,6 +4,7 @@
 
 #Should return a list of BIN files on your System Volume
 #>
+# In windows 11 ver 24H2 - the Get-Volume command returns filesystem like this "FAT" since in the code you specified only "FAT32" it will not work. probeblx it's better to change it to $_.FileSystemType -contains "FAT" with this change it worked for me.
 $Volume = Get-Volume | Where-Object {$_.FileSystemType -eq "FAT32" -and $_.DriveType -eq "Fixed"}
 $SystemDisk = Get-Disk | Where-Object {$_.IsSystem -eq $true}
 $SystemPartition = Get-Partition -DiskNumber $SystemDisk.DiskNumber | Where-Object {$_.IsSystem -eq $true}  
